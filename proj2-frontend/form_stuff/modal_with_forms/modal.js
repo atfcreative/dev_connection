e939403e12cf05d6792ae582de1a92b24c3ac1a0
atfcreative <atfcreative@gmail.com>
2018-11-02 08:51:52 -0700

    adds dev-connect to atf repo

---
+++ proj2-frontend/form_stuff/modal_with_forms/modal.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('#login').click(function(){
+    $('#modalLogin').fadeToggle( 'slow' );
+  });
+  // $('#register').click(function(){
+  //   $('#modalRegister').fadeToggle( 'slow' );
+  // });
+});
